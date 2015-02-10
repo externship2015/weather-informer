a1175f6f0c6c434503aaec85212ee83b3b8cfb04
Maria Malikova <m.malikova73@gmail.com>
2015-02-10 23:15:52 +0300

    commit

--- TheTime/Properties/Settings.Designer.cs
+++ TheTime/Properties/Settings.Designer.cs
@@ -37,8 +37,8 @@ namespace TheTime.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=d:\\Diplom\\TheTime\\TheTime\\DataSet.md" +
-            "f;Integrated Security=True;Connect Timeout=30")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\DataSet.mdf;Integrat" +
+            "ed Security=True;Connect Timeout=30")]
         public string DataSetConnectionString1 {
             get {
                 return ((string)(this["DataSetConnectionString1"]));
